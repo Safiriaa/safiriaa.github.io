d88b4fceecb8e0b748ed43ffc9a0e59ee46b7bfb
Safiriaa <jackiehgou@example.com>
2022-03-23 17:11:53 -0700

    random grid

--- vow/script.js
+++ vow/script.js
@@ -5,16 +5,31 @@ let outputPath = [];
 let outputName = [];
 
 async function getData(){
+    // get data
     const fetchPromise = await fetch('data/data.json');
     const data = await fetchPromise.json();
     for (let i = 0; i < data.symbols.length; i++) {
         name.push(data.symbols[i].name);
         path.push(data.symbols[i].path);
     }
+    // randomly select data to use for grid
     randomIndex();
-    // shuffleOutput(outputPath);
-    // shuffleOutput(outputName);
     document.getElementById('remotedata').innerHTML = outputHTML(data);
+    // ramdomize display
+    let numsName = [1,3,5,7,9,11,13,15,17];
+    let nums = [0,2,4,6,8,10,12,14,16];   
+    nums = shuffleOutput(nums);
+    // update display
+    let gridArray = [document.querySelectorAll('.symbol')];
+    let names = [document.querySelectorAll('.name')];
+    for(let i = 0; i < 9; i++){
+        let randomIndex = nums[i];
+        gridArray[0][i].style.order = randomIndex;
+    }
+    for(let i = 0; i < 9; i++){
+        let randomIndex = numsName[i];
+        names[0][i].style.order = randomIndex;
+    }
 }
 
 function randomIndex(){
